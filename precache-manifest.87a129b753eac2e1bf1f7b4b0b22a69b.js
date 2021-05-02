35fb64cb3469a974a112776e1f9a3ba3b8d624d6
Rishi Datta <rishidatta8@gmail.com>
2021-05-02 22:00:21 +0530

    Updates

---
+++ precache-manifest.87a129b753eac2e1bf1f7b4b0b22a69b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "825fedaf3476e9ab094a9f630e44609c",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "3fc9d53aba8d40b9b9a3",
+    "url": "/keeper-app/static/css/2.dd1866a9.chunk.css"
+  },
+  {
+    "revision": "3fc9d53aba8d40b9b9a3",
+    "url": "/keeper-app/static/js/2.d14a114a.chunk.js"
+  },
+  {
+    "revision": "c17d939579a8a88390ea",
+    "url": "/keeper-app/static/js/main.55351486.chunk.js"
+  },
+  {
+    "revision": "e4ad95b7ee73d89007ed",
+    "url": "/keeper-app/static/js/runtime-main.fc0473d0.js"
+  }
+]);
